47efe22921959a7ef19c6c563b375ff4f9aed283
Priyanshu <pv254424@gmail.com>
2026-07-11 23:04:41 +0530

    feat: implement KyvoraPluginManager with extensible base architecture and initial suite of specialized agent plugins

--- src/vs/workbench/contrib/kyvoraAdvanced/browser/kyvoraPluginManager.ts
+++ src/vs/workbench/contrib/kyvoraAdvanced/browser/kyvoraPluginManager.ts
@@ -429,24 +429,24 @@ export class KyvoraPluginManager extends Disposable {
 			new IamMinifierPlugin(), new FeatureFlagPlugin(), new SecretLeakPlugin(),
 			new ColdStartPlugin(),
 			// Category 6
-			new CWETracerPlugin(), new PrivacyScannerPlugin(), new CryptoAuditorPlugin(),
-			new CVEPatcherPlugin(), new LicenseCheckerPlugin(), new OWASPLinterPlugin(),
+			new CweTracerPlugin(), new PrivacyScannerPlugin(), new CryptoAuditorPlugin(),
+			new CvePatcherPlugin(), new LicenseCheckerPlugin(), new OwaspLinterPlugin(),
 			new ContainerCorrelatorPlugin(), new RateLimitPlugin(), new ThreatModelingPlugin(),
 			new SmartContractPlugin(),
 			// Category 7
 			new NPlusOnePlugin(), new SchemaDriftPlugin(), new MockSeederPlugin(),
-			new RestConverterPlugin(), new QueryTunerPlugin(), new APIDocPlugin(),
-			new IndexRecommendationPlugin(), new SDKGeneratorPlugin(), new MigrationPreviewPlugin(),
-			new WSMockPlugin(),
+			new RestConverterPlugin(), new QueryTunerPlugin(), new ApiDocPlugin(),
+			new IndexRecommendationPlugin(), new SdkGeneratorPlugin(), new MigrationPreviewPlugin(),
+			new WsMockPlugin(),
 			// Category 8
-			new HotPathPlugin(), new GCOverlayPlugin(), new CacheMissPlugin(),
+			new HotPathPlugin(), new GcOverlayPlugin(), new CacheMissPlugin(),
 			new BundleSizePlugin(), new ComplexityPlugin(), new FrameDropPlugin(),
 			new AssetOptimizerPlugin(), new PayloadMinimizerPlugin(), new DistributedTracingPlugin(),
 			new EdgeLatencyPlugin(),
 			// Category 9
 			new TranspilerPlugin(), new DecouplerPlugin(), new AsyncAwaitPlugin(),
 			new TailwindPlugin(), new VersionMigrationPlugin(), new ClassComponentPlugin(),
-			new TypeSafetyPlugin(), new ORMSwitcherPlugin(), new MacroGeneratorPlugin(),
+			new TypeSafetyPlugin(), new OrmSwitcherPlugin(), new MacroGeneratorPlugin(),
 			new DesignPatternPlugin(),
 			// Category 10
 			new TaskDecompositionPlugin(), new HabitAnalyticsPlugin(), new SandboxLearningPlugin(),
