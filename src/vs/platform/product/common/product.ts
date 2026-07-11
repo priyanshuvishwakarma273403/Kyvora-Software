f79de13659a53bbb904f302e17dc86179935779c
Priyanshu <pv254424@gmail.com>
2026-07-11 19:09:42 +0530

    feat: add product configuration module and rebrand product metadata to Kyvora Studio

--- src/vs/platform/product/common/product.ts
+++ src/vs/platform/product/common/product.ts
@@ -62,15 +62,15 @@ else {
 	if (Object.keys(product).length === 0) {
 		Object.assign(product, {
 			version: '1.104.0-dev',
-			nameShort: 'Code - OSS Dev',
-			nameLong: 'Code - OSS Dev',
-			applicationName: 'code-oss',
-			dataFolderName: '.vscode-oss',
-			urlProtocol: 'code-oss',
-			reportIssueUrl: 'https://github.com/microsoft/vscode/issues/new',
+			nameShort: 'Kyvora Studio',
+			nameLong: 'Kyvora Studio',
+			applicationName: 'kyvora-studio',
+			dataFolderName: '.kyvora-studio',
+			urlProtocol: 'kyvora-studio',
+			reportIssueUrl: 'https://github.com/priyanshuvishwakarma273403/Kyvora-Software/issues/new',
 			licenseName: 'MIT',
-			licenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt',
-			serverLicenseUrl: 'https://github.com/microsoft/vscode/blob/main/LICENSE.txt',
+			licenseUrl: 'https://github.com/priyanshuvishwakarma273403/Kyvora-Software/blob/main/LICENSE.txt',
+			serverLicenseUrl: 'https://github.com/priyanshuvishwakarma273403/Kyvora-Software/blob/main/LICENSE.txt',
 			defaultChatAgent: {
 				extensionId: 'GitHub.copilot',
 				chatExtensionId: 'GitHub.copilot-chat',
